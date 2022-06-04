7606f2d34d0ce8e67d57d43771afb284191ea33c
Nikoloz Chitashvili <chitashvili.nikoloz@gmail.com>
2022-06-04 19:36:58 +0400

    Task 9 Done

--- Leetcode - Solutions.playground/Pages/Task 9.xcplaygroundpage/Contents.swift
+++ Leetcode - Solutions.playground/Pages/Task 9.xcplaygroundpage/Contents.swift
@@ -1,3 +1,19 @@
 import Foundation
+let testData: Int = 21
 
-print("Hello")
+class Solution {
+    func isPalindrome(_ x: Int) -> Bool {
+        let str: String = String(x)
+        if str.count == 1 { return true }
+        for iter in 0...str.count/2-1 {
+            let leftChar: Character = str[str.index(str.startIndex, offsetBy: iter)]
+            let rightChar: Character = str[str.index(str.endIndex, offsetBy: -iter-1)]
+            if leftChar != rightChar { return false }
+        }
+        return true
+    }
+}
+
+
+let solution = Solution()
+print(solution.isPalindrome(testData))
